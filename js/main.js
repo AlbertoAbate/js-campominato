771326285589770a8d98e8ae981f97915bfc30a6
Alberto <alberto.abate87@gmail.com>
2020-10-23 17:41:11 +0200

    esercizio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 // campominato
 // il computer deve generare 16 numeri random diversi compresi tra 1 e 100
 var numberList = [];
+var userList = [];
 var size = 16;
+var nMin = 1;
+var nMax = 100;
 
 while (numberList.length < size) {
   var number = getRandomNumber(1, 100);
@@ -15,14 +18,18 @@ console.log(numberList);
 
 // In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L'utente non può inserire più volte lo stesso numero.
-for (var i = 0; i < 5; i++) {
+for (var i = 0; userList.length < 5; i++) {
   var numUser = parseInt (prompt("inserisci un numero"));
+  while (userList.includes(numUser) || ( numUser < nMin) || (numUser > nMax)) {
+    alert("attenzione! numero errato, scrivi un nuovo numero");
+    numUser = parseInt (prompt("inserisci un numero"));
+  }
+  userList.push(numUser);
   if (numberList.includes(numUser)) {
     console.log("Hai preso una mina, hai perso!");
+    break;
   } else if (! numberList.includes(number)) {
     numUser = parseInt (prompt("inserisci un numero"));
-  } else if (i = 5) {
-    console.log("you win!!!");
   }
 }
 
